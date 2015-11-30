9e0c1ab87fbb9ac763f7029ef837d6949f672728
Steve Kondik <steve@cyngn.com>
2015-11-29 20:57:11 -0600

    g3: Drop all the janky proprietary stuff we don't even use
    Change-Id: Ic3bdd30ceaae622d779ced8b8222dc9f795c64c0

--- g3.mk
+++ g3.mk
@@ -156,7 +156,6 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libdashplayer \
     libdivxdrmdecrypt \
     libextmedia_jni \
     libOmxAacEnc \
@@ -165,15 +164,9 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libOmxVdec \
-    libOmxVdecHevc \
     libOmxVenc \
     libOmxVidcCommon \
-    libqcmediaplayer \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
